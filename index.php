de87657e636103dab486b9d058a11be677d2fbdc
Archie Makuwa <archie@aatsol.co.za>
2015-10-28 09:07:26 +0200

    Init commit and a bit of mods

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>TheBillPlease - We develop web applcations</title>
+        
+        <meta name="description" content="TheBillPlease - We develop web applcations. Our website is not quite ready.">
+
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+    
+        <div class="wrapper">
+            
+            <div class="right">
+                <ul>
+                    <li>Hi there.</li>
+                    <li>We&rsquo;re TheBillPlease.</li>
+                    <li>We develop web applications.</li>
+                    <li>Our site&rsquo;s not quite ready yet, though.</li>
+                    <li>Hold tight.</li>
+                </ul>
+
+                <a href="tel:0215562991">0215562991</a>
+                <a href="mailto:hello@thebillplease.co.za">hello@thebillplease.co.za</a>
+                <a href="/">
+                    <img class="logo" src="img/bill.png" title="Follow us on Twitter" alt="TheBillPlease">  
+                </a>
+            </div>
+            
+        </div>
+
+
+        <script src="js/terminal.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-68464937-2', 'auto');
+          ga('send', 'pageview');
+        </script>
+    </body>
+</html>
